b03ea0ca189d98138878c8af1e1d148ffc945f52
Daniel Maděra <madera.dan@gmail.com>
2021-11-23 15:44:40 +0100

    Version 1

---
+++ NemlibToXml.cs
@@ -0,0 +1,131 @@
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Xml.Serialization;
+using System.Xml;
+
+using NEMLIB_OBJ;
+
+
+namespace money_nemlib
+{
+    class NemlibToXml {
+        private List<S5DataObjednavkaPrijata> _objednavky = new List<S5DataObjednavkaPrijata>();
+
+        private string prijatyDoklad;
+
+        public NemlibToXml(string inputFile, Encoding enc) {
+            var rawlines = File.ReadAllLines(inputFile, enc);            
+            var lines = new List<string>();
+            foreach(var line in rawlines) {
+                if(line.Trim() == "") continue;
+                lines.Add(line);
+            }
+
+            if(lines.Count < 4) throw new InvalidDataException("Neplatný formát souboru - chybí kontrolní řádky.");
+
+            prijatyDoklad = lines[0].Trim();
+
+            if(prijatyDoklad == "") throw new InvalidDataException("Neplatný formát souboru - chybí na prvním řádku přijatý doklad.");
+
+            if(!lines[1].StartsWith("===")) throw new InvalidDataException("Neplatný formát souboru - chybí na začátku řádek ===.");
+            if(!lines[2].StartsWith("___")) throw new InvalidDataException("Neplatný formát souboru - chybí řádek oddělující objednávky ___.");
+            if(!lines[lines.Count-1].StartsWith("===")) throw new InvalidDataException("Neplatný formát souboru - chybí na konci řádek ===.");
+
+            lines.RemoveRange(0, 3);
+            lines.RemoveAt(lines.Count-1);
+
+            processFile(lines);
+        }
+
+        private void processFile(List<string> lines) {
+            var obj = new S5DataObjednavkaPrijata();
+            var polozky = new List<S5DataObjednavkaPrijataPolozkyPolozkaObjednavkyPrijate>();
+            int cisloPolozky = 1;
+
+            foreach(var line in lines) {
+                if(line.StartsWith("___")) {
+                    obj.Polozky = new S5DataObjednavkaPrijataPolozky() {
+                        PolozkaObjednavkyPrijate = polozky.ToArray()
+                    };
+                    obj.Odkaz = prijatyDoklad;
+                    _objednavky.Add(obj);
+                    obj = new S5DataObjednavkaPrijata();
+                    polozky = new List<S5DataObjednavkaPrijataPolozkyPolozkaObjednavkyPrijate>();
+                    cisloPolozky = 1;
+                    continue;
+                }
+
+                var values = line.Split("|");
+                if(values.Length != 6) throw new InvalidDataException("Neplatný formát souboru - split |.");
+
+                obj.Adresa = new S5DataObjednavkaPrijataAdresa() {
+                    Firma = new S5DataObjednavkaPrijataAdresaFirma() {
+                        KodOdb_UserData = values[0].Trim()
+                    }
+                };
+
+                obj.AdresaKoncovehoPrijemce = new S5DataObjednavkaPrijataAdresaKoncovehoPrijemce() {
+                    Firma = new S5DataObjednavkaPrijataAdresaKoncovehoPrijemceFirma() {
+                        KodOdb_UserData = values[0].Trim()
+                    }
+                };
+
+                obj.AdresaPrijemceFaktury = new S5DataObjednavkaPrijataAdresaPrijemceFaktury() {
+                    Firma = new S5DataObjednavkaPrijataAdresaPrijemceFakturyFirma() {
+                        KodOdb_UserData = values[0].Trim()
+                    }
+                };
+
+                obj.PrijemceFaktury = new S5DataObjednavkaPrijataPrijemceFaktury() {
+                    KodOdb_UserData = values[0].Trim()
+                };
+
+                obj.KonecnyPrijemce = new S5DataObjednavkaPrijataKonecnyPrijemce() {
+                    KodOdb_UserData = values[0].Trim()
+                };
+
+                polozky.Add(new S5DataObjednavkaPrijataPolozkyPolozkaObjednavkyPrijate() {
+                    CisloPolozky = cisloPolozky.ToString(),
+                    TypObsahu = new enum_TypObsahuPolozky() { Value = enum_TypObsahuPolozky_value.Item1 },
+                    ObsahPolozky = new S5DataObjednavkaPrijataPolozkyPolozkaObjednavkyPrijateObsahPolozky() {
+                        Sklad = new S5DataObjednavkaPrijataPolozkyPolozkaObjednavkyPrijateObsahPolozkySklad() {
+                            Kod = "HL"
+                        },
+                        Artikl = new S5DataObjednavkaPrijataPolozkyPolozkaObjednavkyPrijateObsahPolozkyArtikl() {
+                            Kod = values[3].Trim()
+                        }
+                    },
+                    Mnozstvi = values[4].Trim()
+                });
+
+                cisloPolozky++;
+            }
+
+            obj.Polozky = new S5DataObjednavkaPrijataPolozky() {
+                PolozkaObjednavkyPrijate = polozky.ToArray()
+            };
+            obj.Odkaz = prijatyDoklad;
+            _objednavky.Add(obj);
+        }
+
+        public void serialize(string outputFile) {
+            var xmlWriterSettings = new XmlWriterSettings() { 
+                Indent = true,
+                CheckCharacters = false,
+            };
+            var serializer = new XmlSerializer(typeof(S5Data));
+            using (var stream = XmlWriter.Create(outputFile, xmlWriterSettings)) {
+                var data = new S5Data() {
+                    ObjednavkaPrijataList = _objednavky.ToArray()
+                };
+                serializer.Serialize(stream, data, null, "");
+            }
+            
+            string text = File.ReadAllText(outputFile);
+            text = text.Replace("&amp;#13;&amp;#10;" , "&#xD;&#xA;");
+            File.WriteAllText(outputFile, text);
+        }
+
+    }    
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace money_nemlib
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.WriteLine("{0:yyyy-MM-dd HH:mm:ss} Converze nemlib txt objednávky do XML.", DateTime.Now);
+            try {
+                if (args.Length != 2 && args.Length != 1) throw new ArgumentException("Chyba vstupních argumentů. Povolený počet: 1 nebo 2.");
+
+                string inputTxtFile = args[0];
+                string outputTxtFile = inputTxtFile;
+                if(args.Length == 2) {
+                    outputTxtFile = args[1];
+                } else {
+                    makeCopy(inputTxtFile);
+                }
+            
+                if (!File.Exists(inputTxtFile)) throw new ArgumentException($"Nepodařilo se načíst vstupní XML soubor: {inputTxtFile}.");
+
+                var conv = new NemlibToXml(inputTxtFile, System.Text.Encoding.UTF8);
+                conv.serialize(outputTxtFile);
+                return 0;
+
+            } catch(Exception e) {
+                Console.WriteLine(e.Message);
+                Console.ReadKey();
+                return 1;
+            }
+        }
+
+        private static void makeCopy(string file) {
+            string fname = Path.GetFileNameWithoutExtension(file);
+            string newNameBase = fname + "_", newFile = "";
+            int found = 0;
+            var r = new Regex("^(.+_)([0-9]{1,3})$");
+            if(r.IsMatch(fname)) {
+                Match match = r.Match(fname);
+                string grpMatch = match.Groups[2].ToString();
+                found = int.Parse(grpMatch);
+                newNameBase = match.Groups[1].ToString();
+            }
+            while(true) {
+                newFile = Path.Combine(Path.GetDirectoryName(file), newNameBase + (found+1).ToString() + Path.GetExtension(file));
+                if(!File.Exists(newFile)) {
+                    File.Copy(file, newFile);
+                    break;
+                }
+                found++;
+            }
+            
+        }
+    }
+}
